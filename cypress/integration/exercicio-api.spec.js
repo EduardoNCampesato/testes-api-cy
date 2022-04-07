933cc7bf900d1c50e806e3b182c35a5e78db7714
Eduardo <eduardo.campesato.7@gmail.com>
2022-04-07 10:36:37 -0300

    Exercício concluído

--- cypress/integration/exercicio-api.spec.js
+++ cypress/integration/exercicio-api.spec.js
@@ -7,57 +7,18 @@ describe('Testes da Funcionalidade Usuários', () => {
           cy.token('eduardo.negrao.7@qa.com.br', 'teste').then(tkn =>{token})
      });
 
-     it.only('Deve validar contrato de usuários', () => {
+     it('Deve validar contrato de usuários', () => {
           cy.request('usuarios').then(response => {
                return contrato.validateAsync(response.body)
           }) 
      });
 
-     it.only('Deve listar usuários cadastrados', () => {
+     it('Deve listar usuários cadastrados', () => {
           cy.request({
                method: 'GET',
-               url: 'usuarios',
-               body: {
-                    "quantidade": 3,
-                    "usuarios": [
-                         {
-                              "nome": "Fulano Souza",
-                              "email": "ciclano@qa.com.br",
-                              "password": "teste1",
-                              "administrador": "true",
-                              "_id": "6qNcZqYsGjLJFVi1"
-                            },
-                            {
-                              "nome": "Eduardo Campesato",
-                              "email": "eduardo.campesato.7@qa.com.br",
-                              "password": "teste",
-                              "administrador": "true",
-                              "_id": "IrLCe9gRkXqNDUna"
-                            },
-                            {
-                              "nome": "Rafael Silva",
-                              "email": "rafael.silva@qa.com.br",
-                              "password": "teste",
-                              "administrador": "true",
-                              "_id": "ahH3ppWHBluq6xad"
-                            },
-                            {
-                              "nome": "Eduardo Negrão",
-                              "email": "eduardo.negrao.7@qa.com.br",
-                              "password": "teste",
-                              "administrador": "true",
-                              "_id": "gItaQgLXY4bjR7UG"
-                            },
-                            {
-                              "nome": "Rafael Silva",
-                              "email": "rafael.silv@qa.com.br",
-                              "password": "teste",
-                              "administrador": "true",
-                              "_id": "gY4vjlAcWfwPIoPd"
-                            }
-                    ]
-               }
-
+               url: 'usuarios',          
+          }).then(response =>{
+               expect(response.status).to.equal(200)
           })
      });
 
@@ -67,7 +28,7 @@ describe('Testes da Funcionalidade Usuários', () => {
                url: 'usuarios',
                body: {
                     "nome": "Rafael Souza",
-                    "email": "rafael.souza0002@qa.com.br",
+                    "email": "rafael.souza.teste002@qa.com.br",
                     "password": "teste",
                     "administrador": "true"
                },
@@ -108,8 +69,8 @@ describe('Testes da Funcionalidade Usuários', () => {
                     headers: {authorization: token},
                     body: 
                     {
-                         "nome": "Eduardo Campesato 002",
-                         "email": "eduardo.negrao.002@qa.com.br",
+                         "nome": "Eduardo Campesato 00",
+                         "email": "eduardo.negrao.00@qa.com.br",
                          "password": "teste",
                          "administrador": "true"
                     }
